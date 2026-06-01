f6c1776b2473545d66605487495753c1f182412c
mahi999-max <mahimr9999@gmail.com>
2026-06-01 17:46:36 +0600

    fix fetch URL

--- src/app/Provider/ContextProvider.jsx
+++ src/app/Provider/ContextProvider.jsx
@@ -8,7 +8,7 @@ const ContextProvider = ({ children }) => {
     const [users, setUsers] = useState([])
     useEffect(() => {
         const fetchData = async () => {
-            const res = await fetch('http://localhost:3000/data.json')
+            const res = await fetch('/data.json')
             const data = await res.json()
             setUsers(data)
         }
